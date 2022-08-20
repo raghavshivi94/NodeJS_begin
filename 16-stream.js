e86a0f6ca264dcfca055be30e072da1974babff3
Shivali <raghavshivi94@gmail.com>
2022-08-20 11:34:39 +0530

    event related topics

--- 16-stream.js
+++ 16-stream.js
@@ -1,12 +1,11 @@
 const { createReadStream } = require('fs')
 
-const stream = createReadStream('./content/big.txt', { highWaterMark: 90000, encoding: 'utf8'})
 // default 64kb
 // last buffer - remainder
 // highWaterMark - control size
 // const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
 // const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
-// const stream = createReadStream('./content/big.txt')
+const stream = createReadStream('./content/big.txt')
 
 stream.on('data', (result) => {
   console.log(result)
